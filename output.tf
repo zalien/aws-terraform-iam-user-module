eff6fe8e3507459317e80990550a85c2a2f2d109
Sergio Teixeira <sergiopintoteixeira@gmail.com>
2020-02-24 15:12:46 +0100

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,20 +1,3 @@
-output "user_secret" {
-  value = {
-    for secret in aws_iam_access_key.user_key :
-    secret.secret => secret.secret
-  }
-}
-
-output "user_key" {
-  value = {
-    for key in aws_iam_access_key.user_key :
-    key.key => key.key
-  }
-}
-
 output "user" {
-  value = {
-    for user in aws_iam_access_key.user_key:
-    key.key => key.key
-  }
+  value = [for user in aws_iam_access_key.user_key : user]
 }
